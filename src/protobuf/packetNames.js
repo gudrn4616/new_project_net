528861fefce03a3720430e9d9878faaa3d2483c4
rabbit-97 <uzam97@gmail.com>
2024-11-04 17:20:41 +0900

    feat: 프로토콜 파일 생성
     - 몬스터 생성 요청
     - 몬스터 생성 응답
     - 적 몬스터 생성 알림
     - 몬스터의 기지 공격 요청
     - 기지 체력 업데이트 알림

--- src/protobuf/packetNames.js
+++ src/protobuf/packetNames.js
@@ -1,5 +1,21 @@
 export const packetNames = {
   common: {
     packetHeader: 'common.Packet',
+    position: 'common.Position',
+    baseData: 'common.BaseData',
+    towerData: 'common.TowerData',
+    monsterData: 'common.MonsterData',
+    initialGameState: 'common.InitialGameState',
+    gameState: 'common.GameState',
+  },
+  monsterNotification: {
+    spawnEnemyMonsterNotification: 'monsterNotification.S2CSpawnEnemyMonsterNotification',
+  },
+  request: {
+    spawnMonsterRequest: 'request.C2SSpawnMonsterRequest',
+    monsterAttackBaseRequest: 'request.C2SMonsterAttackBaseRequest',
+  },
+  response: {
+    spawnMonsterResponse: 'response.S2CSpawnMonsterResponse',
   },
 };
