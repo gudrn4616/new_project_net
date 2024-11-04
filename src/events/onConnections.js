4ff209aa32b4f7369aa98bf08691c52b6916fcea
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-04 13:57:39 +0900

    Update onConnections.js

--- src/events/onConnections.js
+++ src/events/onConnections.js
@@ -7,7 +7,7 @@ export const onConnections = (socket) => {
 
   socket.buffer = Buffer.alloc(0);
 
-  socket.on('data', onDate(socket));
+  socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
   socket.on('error', onError(socket));
 };
