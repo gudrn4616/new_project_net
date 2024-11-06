efbc4e6d95e7db713cd7471759c389078fb9ebd8
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-06 23:19:26 +0900

    Fix: camel case 변환 적용

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -1,14 +1,13 @@
 import pools from '../database.js';
 import { SQL_QUERIES } from './user.query.js';
+import { toCamelCase } from '../../utils/transformCase.js';
 
-//유저 생성
 export const createUser = async (email, userId, password) => {
   await pools.USER_DB.query(SQL_QUERIES.CREATE_USER, [email, userId, password]);
 };
 
-//유저 확인
 export const findUserById = async (userId) => {
   const [rows] = await pools.USER_DB.query(SQL_QUERIES.FIND_USER_BY_ID, [userId]);
 
-  return rows[0];
+  return toCamelCase(rows[0]);
 };
