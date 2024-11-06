3a8b19348ca346a65b4e22163a9d4ae95aab3bd2
rabbit-97 <uzam97@gmail.com>
2024-11-06 20:30:40 +0900

    fix: 게임 세션 불필요한 로직 제거 및 수정

--- src/session/game.session.js
+++ src/session/game.session.js
@@ -1,7 +1,4 @@
-// sessionManager.js
-import Game from '../class/models/game.class.js';
-import Monster from '../class/models/monster.class.js';
-import Base from '../class/models/base.class.js';
+import Game from '../classes/models/game.classes.js';
 import { gameSessions } from './sessions.js';
 
 // 게임 세션 추가
@@ -28,55 +25,31 @@ export const getGameSession = (socket) => {
   );
 };
 
-// 몬스터 추가
-export const addMonsterToSession = (socket, monsterData) => {
+// 게임 세션에 몬스터 추가
+export const addMonsterToSession = (socket, monster) => {
   const session = getGameSession(socket);
   if (session) {
-    const { id, name, health, attack } = monsterData;
-    const newMonster = new Monster(id, name, health, attack);
-    session.addMonster(newMonster);
-    return newMonster;
-  } else {
-    throw new Error('Game session not found');
+    session.monsters.push(monster);
   }
 };
 
-// 몬스터 제거
+// 게임 세션에서 몬스터 삭제
 export const removeMonsterFromSession = (socket, monsterId) => {
   const session = getGameSession(socket);
   if (session) {
-    session.removeMonster(monsterId);
-  }
-  return session;
-};
-
-// 몬스터 조회
-export const getMonsterFromSession = (socket, monsterId) => {
-  const session = getGameSession(socket);
-  if (session) {
-    return session.getMonster(monsterId);
-  }
-  return null;
-};
-
-// 기지 추가
-export const addBaseToSession = (socket, baseData) => {
-  const session = getGameSession(socket);
-  if (session) {
-    const { id, name, health } = baseData;
-    const newBase = new Base(id, name, health);
-    session.addBase(newBase);
-    return newBase;
-  } else {
-    throw new Error('Game session not found');
+    const index = session.monsters.findIndex((monster) => monster.id === monsterId);
+    if (index !== -1) {
+      return session.monsters.splice(index, 1)[0];
+    }
   }
 };
 
-// 기지 조회
-export const getBaseFromSession = (socket, baseId) => {
+// 기지 체력 감소
+export const reduceBaseHealthInSession = (socket, damage) => {
   const session = getGameSession(socket);
   if (session) {
-    return session.getBase(baseId);
+    session.reduceBaseHealth(damage);
+    return session.getBaseHealth();
   }
   return null;
 };
