1c60f1fb47e57a68d91d8df5b93321b4b4359fb1
rabbit-97 <uzam97@gmail.com>
2024-11-06 20:30:15 +0900

    feat: 기존 핸들러 인덱스 로직 추가

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,19 +1,30 @@
 import { HANDLER_IDS } from '../constants/handlerIds.js';
-import updateBaseHandler from './base/updateBase.handler.js';
-import monsterAttackHandler from './monster/monsterAttack.handler.js';
-import monsterSpawnHandler from './monster/monsterSpawn.handler.js';
+// 기존 핸들러들
+import spawnMonsterRequestHandler from './request/spawnMonsterRequest.handler.js';
+import spawnMonsterResponseHandler from './response/spawnMonsterResponse.handler.js';
+import spawnEnemyMonsterNotificationHandler from './notification/spawnEnemyMonsterNotification.handler.js';
+import monsterAttackBaseRequestHandler from './request/monsterAttackBaseRequest.handler.js';
+import updateBaseHPNotificationHandler from './notification/updateBaseHPNotification.handler.js';
 
 const handlers = {
-  [HANDLER_IDS.MONSTER_SPAWN]: {
-    handler: monsterSpawnHandler,
+  [HANDLER_IDS.SPAWN_MONSTER_REQUEST]: {
+    handler: spawnMonsterRequestHandler,
     protoType: 'request.C2SSpawnMonsterRequest',
   },
-  [HANDLER_IDS.MONSTER_ATTACK]: {
-    handler: monsterAttackHandler,
+  [HANDLER_IDS.SPAWN_MONSTER_RESPONSE]: {
+    handler: spawnMonsterResponseHandler,
+    protoType: 'response.S2CSpawnMonsterResponse',
+  },
+  [HANDLER_IDS.SPAWN_ENEMY_MONSTER_NOTIFICATION]: {
+    handler: spawnEnemyMonsterNotificationHandler,
+    protoType: 'notification.S2CSpawnEnemyMonsterNotification',
+  },
+  [HANDLER_IDS.MONSTER_ATTACK_BASE_REQUEST]: {
+    handler: monsterAttackBaseRequestHandler,
     protoType: 'request.C2SMonsterAttackBaseRequest',
   },
-  [HANDLER_IDS.UPDATE_BASE]: {
-    handler: updateBaseHandler,
+  [HANDLER_IDS.UPDATE_BASE_HP_NOTIFICATION]: {
+    handler: updateBaseHPNotificationHandler,
     protoType: 'notification.S2CUpdateBaseHPNotification',
   },
 };
