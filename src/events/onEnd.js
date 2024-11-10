6ee29ff798350dd5b7a715191595ff523ccc9b0c
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-10 22:08:03 +0900

    Fix: 인터벌 종료 수정 중

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,3 +1,15 @@
+import { getGameSession } from '../session/game.session.js';
+
 export const onEnd = (socket) => async () => {
-  console.log('Connection closed');
+  const gameSession = getGameSession(socket);
+  if (!gameSession) {
+    throw new Error('해당 유저의 게임 세션이 존재하지 않습니다.');
+  }
+
+  const user = gameSession.users.find((user) => user.socket === socket);
+  if (!user) {
+    throw new Error('유저가 존재하지 않습니다.');
+  }
+
+  // interval 종료하기
 };
