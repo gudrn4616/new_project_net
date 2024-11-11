b701c9fbf88e00272810a335da366fa82ebbd8e6
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-11 13:55:27 +0900

    Feat: 게임 종료 로직

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,5 +1,24 @@
-import { exitGameSession } from '../session/game.session.js';
+import { endGameHandler } from '../handler/game/endGame.handler.js';
+import {
+  exitGameSession,
+  getEndGameQueue,
+  getInGameUsers,
+  getWaitingQueue,
+  removeWaitingQueue,
+} from '../session/game.session.js';
+import { getUser, removeUser } from '../session/user.session.js';
 
 export const onEnd = (socket) => async () => {
+  const user = getUser(socket);
+
+  if (getWaitingQueue().has(user)) {
+    removeWaitingQueue(user);
+  }
+
+  if (getEndGameQueue().has(user) || getInGameUsers().has(user)) {
+    endGameHandler(socket, 1);
+  }
+
   exitGameSession(socket);
+  removeUser(socket);
 };
---
+++ src/handler/game/endGame.handler.js
@@ -0,0 +1,85 @@
+import { createWOL, findWOLById, updateWOL } from '../../db/game/wol.db.js';
+import {
+  addEndGameQueue,
+  getEndGameQueue,
+  getGameSessionbyUser,
+  getInGameUsers,
+  removeEndGameQueue,
+  removeGameSessionbyUser,
+  removeInGameUser,
+} from '../../session/game.session.js';
+import { getUser } from '../../session/user.session.js';
+
+// 게임 종료 처리
+export const endGameHandler = async (socket, trigger = 0) => {
+  const currentUser = getUser(socket);
+  if (!currentUser) return;
+
+  const gameSession = getGameSessionbyUser(currentUser);
+  if (!gameSession) return;
+
+  const opponent = gameSession.users.find((user) => user.id !== currentUser.id);
+
+  addEndGameQueue(currentUser);
+  addEndGameQueue(opponent);
+
+  let endGameQueue = getEndGameQueue();
+
+  if (endGameQueue.has(currentUser) && endGameQueue.has(opponent)) {
+    let isCurrentUserWin, isOpponentWin;
+
+    if (trigger === 0) {
+      const currentUserHp = gameSession.baseHp[currentUser.id];
+      const opponentHp = gameSession.baseHp[opponent.id];
+      isCurrentUserWin = opponentHp <= 0;
+      isOpponentWin = currentUserHp <= 0;
+    } else if (trigger === 1) {
+      isCurrentUserWin = false;
+      isOpponentWin = true;
+    }
+
+    [currentUser, opponent].forEach((user) => {
+      if (getInGameUsers().has(user)) removeInGameUser(user);
+      removeEndGameQueue(user);
+    });
+
+    removeGameSessionbyUser(currentUser);
+
+    const responses = [currentUser, opponent].map((user, index) => {
+      const isWin = index === 0 ? isCurrentUserWin : isOpponentWin;
+      return createResponse({ isWin }, user, PacketType.GAME_OVER_NOTIFICATION);
+    });
+
+    currentUser.socket.write(responses[0]);
+    opponent.socket.write(responses[1]);
+
+    // 게임 결과 기록
+    let currentUserWOL = await findWOLById(currentUser.id);
+    let opponentWOL = await findWOLById(opponent.id);
+
+    if (!currentUserWOL) {
+      await createWOL(currentUser.id, 0, 0);
+      currentUserWOL = { victory_count: 0, defeat_count: 0 };
+    }
+    if (!opponentWOL) {
+      await createWOL(opponent.id, 0, 0);
+      opponentWOL = { victory_count: 0, defeat_count: 0 };
+    }
+
+    if (isCurrentUserWin) {
+      await updateWOL(
+        currentUser.id,
+        currentUserWOL.victory_count + 1,
+        currentUserWOL.defeat_count,
+      );
+      await updateWOL(opponent.id, opponentWOL.victory_count, opponentWOL.defeat_count + 1);
+    } else if (isOpponentWin) {
+      await updateWOL(
+        currentUser.id,
+        currentUserWOL.victory_count,
+        currentUserWOL.defeat_count + 1,
+      );
+      await updateWOL(opponent.id, opponentWOL.victory_count + 1, opponentWOL.defeat_count);
+    }
+  }
+};
